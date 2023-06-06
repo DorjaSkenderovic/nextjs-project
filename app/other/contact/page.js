3ca6f4627de963821acb78f9c9e5e346bafd2d1b
Dorja Skenderovic <dorjaskenderovic@gmail.com>
2023-06-06 15:51:34 +0200

    finished project

---
+++ app/other/contact/page.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ContactCard from "@/components/ContactCard/ContactCard";
+import styles from "./page.module.scss";
+const contact = () => {
+  return (
+    <div className={styles.contact}>
+      <ContactCard image="phone.png" title="Telefon" contact="+123456789" />
+      <ContactCard image="email.png" title="Email" contact="lorem@ipsum.com" />
+      <ContactCard
+        image="location.png"
+        title="Lokacija"
+        contact={"Ulica br. 11\n Negdje Tamo, 12345"}
+      />
+    </div>
+  );
+};
+
+export default contact;
---
+++ components/Card/Card.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./Card.module.scss";
+import Image from "next/image";
+
+const Card = (props) => {
+  return (
+    <div className={styles.card}>
+      <h2>{props.title}</h2>
+      <Image
+        src={`/assets/${props.image}`}
+        width={350}
+        height={250}
+        alt={props.title}
+        className={styles.image}
+      />
+      <p className={styles.description}>{props.description}</p>
+    </div>
+  );
+};
+
+export default Card;
