5f7505837ff3e87cda054b2d357748949489af93
Dorja Skenderovic <dorjaskenderovic@gmail.com>
2023-06-08 11:47:19 +0200

    Import images in contact page

--- app/other/contact/page.js
+++ app/other/contact/page.js
@@ -1,12 +1,16 @@
 import ContactCard from "@/components/ContactCard/ContactCard";
 import styles from "./page.module.scss";
+import PhoneImage from "../../../public/assets/phone.png";
+import EmailImage from "../../../public/assets/email.png";
+import LocationImage from "../../../public/assets/location.png";
+
 const contact = () => {
     return (
         <div className={styles.contact}>
-            <ContactCard image="phone.png" title="Telefon" contact="+123456789" className={styles.card} />
-            <ContactCard image="email.png" title="Email" contact="lorem@ipsum.com" className={styles.card} />
+            <ContactCard image={PhoneImage} title="Telefon" contact="+123456789" className={styles.card} />
+            <ContactCard image={EmailImage} title="Email" contact="lorem@ipsum.com" className={styles.card} />
             <ContactCard
-                image="location.png"
+                image={LocationImage}
                 title="Lokacija"
                 contact={"Ulica br. 11\n Negdje Tamo, 12345"}
                 className={styles.card}
