dae044b61cb9537ef56c26490df29457e3b53626
Dorja Skenderovic <dorjaskenderovic@gmail.com>
2023-06-13 13:55:00 +0200

    fix ContactCard

--- components/ContactCard/ContactCard.js
+++ components/ContactCard/ContactCard.js
@@ -6,16 +6,18 @@ const ContactCard = (props) => {
     const className = props.className || "";
     return (
         <div className={`${styles.card} ${className}`}>
-            <div className={styles.image}>
-                <Image
-                    src={props.image}
-                    layout="responsive"
-                    alt={props.title}
-                    priority={true}
-                />
+            <div className={styles.head}>
+                <div className={styles.image}>
+                    <Image
+                        src={props.image}
+                        layout="responsive"
+                        alt={props.title}
+                        priority={true}
+                    />
+                </div>
                 <h2>{props.title}</h2>
             </div>
-            <div>
+            <div className={styles.infoData}>
                 {props.contact.split("\n").map((paragraph) => {
                     return <p>{paragraph}</p>;
                 })}
